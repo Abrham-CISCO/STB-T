59d2873137bb6de5902847dad64dfe14c50b9570
Abrham-CISCO <58417725+Abrham-CISCO@users.noreply.github.com>
2020-09-23 09:09:59 +0300

    Updating course.js model
    This course model has been modified to include timestamps, book subdocuments and course outline and created by fields

--- Workspaces/SierateTimhert/models/course.js
+++ Workspaces/SierateTimhert/models/course.js
@@ -1,15 +1,27 @@
 var mongoose = require('mongoose');
+var booksSchema = new mongoose.Schema({
+  bookName:{type:String},
+  filePath:{type:String}
+},{timestamps:true})
 var courseSchema = new mongoose.Schema({
     name:{
         type:String,
         unique:true,
         required:true
     },
+    createdBy:{
+        type:String,
+        required:true
+    },
     description:{
         type:String,
         default:"No description!"
-    }
-})
+    },
+    courseOutline:{
+        type:String
+    },
+    books:[booksSchema]
+},{timestamps:true})
 
 var course = mongoose.model('course',courseSchema);
 module.exports = course
