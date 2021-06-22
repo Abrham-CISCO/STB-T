6931bda1c69f4129e5366714b83db04171d5191b
Abrham Getachew <abrhamcisco@gmail.com>
2021-06-22 12:57:30 -0300

    Last Fix: P1

--- Account/config.js
+++ Account/config.js
@@ -1,7 +1,7 @@
 module.exports = {
     'secretKey':'12345-67890-09876-56123',
- //   'mongoUrl':'mongodb://localhost:27017/S'
-     'mongoUrl':'mongodb+srv://Abrham_Getachew:AGRS0923276844@cluster0.ined8.mongodb.net/S?retryWrites=true&w=majority'
+    'mongoUrl':'mongodb://localhost:27017/S'
+ //    'mongoUrl':'mongodb+srv://Abrham_Getachew:AGRS0923276844@cluster0.ined8.mongodb.net/S?retryWrites=true&w=majority'
 }
 
 // 'mongoUrl':'mongodb+srv://Abrham_Getachew:AGRS0923276844@cluster0.ined8.mongodb.net/S?retryWrites=true&w=majority'
